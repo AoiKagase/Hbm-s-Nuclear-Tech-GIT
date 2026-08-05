66523738e32e04d18efb7b9f1d38df5e9f38a753
Aoi Kagase <datumoujidai@gmail.com>
2026-08-05 15:03:42 +0900

    Optimize demon lamp radiation ray tracing

--- src/main/java/com/hbm/tileentity/machine/TileEntityDemonLamp.java
+++ src/main/java/com/hbm/tileentity/machine/TileEntityDemonLamp.java
@@ -2,7 +2,6 @@ package com.hbm.tileentity.machine;
 
 import java.util.List;
 
-import com.hbm.render.amlfrom1710.Vec3;
 import com.hbm.util.ContaminationUtil;
 import com.hbm.util.ContaminationUtil.ContaminationType;
 import com.hbm.util.ContaminationUtil.HazardType;
@@ -13,7 +12,7 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockPos.MutableBlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -32,24 +31,33 @@ public class TileEntityDemonLamp extends TileEntity implements ITickable {
 
 		float rads = 100000F;
 		double range = 25D;
+		double sourceX = x + 0.5D;
+		double sourceY = y + 0.5D;
+		double sourceZ = z + 0.5D;
+		MutableBlockPos blockPos = new MutableBlockPos();
 
-		List<EntityLivingBase> entities = world.getEntitiesWithinAABB(EntityLivingBase.class, new AxisAlignedBB(x + 0.5, y + 0.5, z + 0.5, x + 0.5, y + 0.5, z + 0.5).grow(range, range, range));
+		List<EntityLivingBase> entities = world.getEntitiesWithinAABB(EntityLivingBase.class, new AxisAlignedBB(sourceX, sourceY, sourceZ, sourceX, sourceY, sourceZ).grow(range, range, range));
 		for(EntityLivingBase e : entities) {
 
-			Vec3 vec = Vec3.createVectorHelper(e.posX - (x + 0.5), (e.posY + e.getEyeHeight()) - (y + 0.5), e.posZ - (z + 0.5));
-			double len = vec.length();
-			vec = vec.normalize();
+			double dx = e.posX - sourceX;
+			double dy = e.posY + e.getEyeHeight() - sourceY;
+			double dz = e.posZ - sourceZ;
+			double distanceSquared = dx * dx + dy * dy + dz * dz;
+			double len = Math.sqrt(distanceSquared);
+			double inverseLength = len >= 1.0E-4D ? 1.0D / len : 0.0D;
+			double nx = dx * inverseLength;
+			double ny = dy * inverseLength;
+			double nz = dz * inverseLength;
 
 			float res = 0;
 
 			for(int i = 1; i < len; i++) {
 
-				int ix = (int)Math.floor(x + 0.5 + vec.xCoord * i);
-				int iy = (int)Math.floor(y + 0.5 + vec.yCoord * i);
-				int iz = (int)Math.floor(z + 0.5 + vec.zCoord * i);
-				
-				BlockPos pos = new BlockPos(ix, iy, iz);
-				IBlockState state = world.getBlockState(pos);
+				int ix = (int)Math.floor(sourceX + nx * i);
+				int iy = (int)Math.floor(sourceY + ny * i);
+				int iz = (int)Math.floor(sourceZ + nz * i);
+
+				IBlockState state = world.getBlockState(blockPos.setPos(ix, iy, iz));
 				res += state.getBlock().getExplosionResistance(null);
 			}
 
@@ -58,7 +66,7 @@ public class TileEntityDemonLamp extends TileEntity implements ITickable {
 
 			float eRads = rads;
 			eRads /= (float)res;
-			eRads /= (float)(len * len);
+			eRads /= (float)distanceSquared;
 
 			ContaminationUtil.contaminate(e, HazardType.RADIATION, ContaminationType.CREATIVE, eRads);
 			if(len < 2) {
